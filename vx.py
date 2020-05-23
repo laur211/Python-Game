9471685862a01868430611f62b91173f0bd02eca
Laurentiu Badiu <badiulaurentiu21@yahoo.com>
2020-05-23 18:14:33 +0300

    Done

---
+++ vx.py
@@ -0,0 +1,62 @@
+
+
+nums=[1,2,3,4,5]
+vals=nums[-3:4]
+print(nums)
+print(vals)
+
+# lst=[1, 2, 3]
+# for v in range(len(lst)):
+#     lst.insert(0, lst[v])
+# print(lst)
+
+
+#
+# i=1
+#
+# while i<10:
+#     print("$")
+#     i=i<<1
+
+
+#
+#
+# lst=[1,2,3,4]
+# print(lst[-3:-2])
+
+# vals=[0,1,2]
+#
+# vals[0],vals[2]=vals[2],vals[0]
+# print(vals)
+
+
+
+# nums=[1,2,3]
+# vals=nums
+# del(vals[1:2])
+# print(vals)
+# print(nums)
+
+#
+# lst=[i for i in range(-1,2)]
+# print(lst)
+#
+# x=1
+# x=x==x
+# print(x)
+
+
+# vals=[0, 1, 2]
+# vals.insert(0,1)
+# print(vals)
+# del vals[1]
+# print(vals)
+
+# p=True
+# q=True
+#
+# print(not (p and q) == (not p) or (not q))
+
+
+
+
